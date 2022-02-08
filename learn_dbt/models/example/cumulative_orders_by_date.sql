6f27b9fc2ce917b9c4b84676227f3e3b7d911383
Sam Keeler <keelersam@gmail.com>
2022-02-08 12:09:53 -0600

    init commit

---
+++ learn_dbt/models/example/cumulative_orders_by_date.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+with daily as
+(select o_orderdate, sum(o_totalprice) as daily_sales
+from snowflake_sample_data.tpch_sf1.orders
+group by o_orderdate)
+
+select o_orderdate, daily_sales, sum(daily_sales) over (order by o_orderdate rows between unbounded preceding and current row) as accum_sales
+from daily
+order by o_orderdate
